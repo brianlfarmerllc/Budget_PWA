55dd9cf42c1219ac9140c567b7d62301f0850997
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-06-15 21:28:38 -0500

    create maifest and service worker

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
+// requireing packages
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
+// port on local host and heroku deployment 
 const PORT = process.env.PORT || 3030;
-
+// create instance of express to use
 const app = express();
-
+// using morgan, compression, url encoding, and parsing json
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+// creating static access to public folder
 app.use(express.static("public"));
-
+// connecting to mongo locally and with heroku
 mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
-// routes
+// routes folder reference to apis
 app.use(require("./routes/api.js"));
-
+// server listening
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
